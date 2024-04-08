54b7914e4d92973f33ba780d346da4eda167983e
RoniePetersonDF2 <126265898+RoniePetersonDF2@users.noreply.github.com>
2024-04-08 17:16:29 -0300

    Create ContaEspecial.php

---
+++ src/ContaEspecial.php
@@ -0,0 +1,32 @@
+<?php
+
+class ContaEspecial extends Conta
+{
+    protected float $limite;
+    public function __construct(float $saldo, float $limite)
+    {
+        $this->saldo = $saldo;
+        $this->limite = $limite;
+    }
+
+    public function getSaldo(): float 
+    {
+        return $this->saldo + $this->limite;
+    }
+    public function sacar(float $valor): void
+    {
+        if ($valor > $this->getSaldo()) {
+            echo "Valor a ser sacado, R$ $valor, é maior que o saldo.\n";
+            return;
+        }
+        $this->saldo -= $valor;
+        echo "Valor a ser sacado: R$ $valor.\n";
+
+    }
+
+    public function imprimirSaldo(): void
+    {
+        $saldo = number_format($this->getSaldo(), 2, ',', '.');
+        echo "Saldo atual é: R$ $saldo\n";
+    }
+}
