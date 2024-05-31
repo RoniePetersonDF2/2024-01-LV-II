6c718734ed894aa935f3271f9853266ce218140b
Ronie Peterson <roniepetersondf@gmail.com>
2024-05-31 18:11:09 -0300

    Update cart.php

--- etc_2024_LV_II/21/cart.php
+++ etc_2024_LV_II/21/cart.php
@@ -1,18 +1,50 @@
 <?php
 session_start();
-var_dump($_SESSION); exit;
 if (!isset($_SESSION["usuario"])) {
     header('Location: index.php?message=Carrinho vazio');
     exit;
 }
 
-$cart = $_SESSION["carrinho"]['']
+$carrinho = $_SESSION['usuario']['carrinho'] ?? [];
+$count = 0;
 ?>
 <?php include 'inc/header.php'; ?>
 
 <main class="container">
     <h1>Carrinho</h1>
+
+    <table class="table table-stripped">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Produto</th>
+                <th>Preço</th>
+                <th>Ação</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php if (count($carrinho) > 0) : ?>
+                <?php foreach ($carrinho as $item) : $count++; ?>
+                    <tr>
+                        <td><?= $count; ?></td>
+                        <td><?= $item['nome']; ?></td>
+                        <td><?= $item['preco']; ?></td>
+                        <td>
+                            <a class="btn btn-success" href="cart-remove.php?id=<?=$item['id'];?>">Remover</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php else : ?>
+                <tr>
+                    <td colspan="4">Não existem produtos no carrinho.</td>
+                </tr>
+            <?php endif; ?>
+        </tbody>
+    </table>
     
+    <br>
+
+    <a href="index.php">Home</a>
 </main>
 
 <?php include 'inc/footer.php'; ?>
