a4934648b8b80ef624e5965145fbd1f76e92a2af
Ronie Peterson <roniepetersondf@gmail.com>
2024-06-05 17:59:06 -0300

    Update cart-show.php

--- etc_2024_LV_II/21/carrinho/cart-show.php
+++ etc_2024_LV_II/21/carrinho/cart-show.php
@@ -1,9 +1,51 @@
+<?php
+include '../config.php';
+include '../src/models/dao/produtodao.php';
+
+$id = $_GET['id'] ?? 0;
+
+$dao = new ProdutoDAO($array_produtos);
+$produto = $dao->getById($id);
+
+if ($produto == false) {
+    header('Location: ../index.php?message=Produto não encontrado!&type=error');
+    exit;
+}
+
+$preco = number_format($produto['preco'], 2,',', '.');
+
+?>
+
 <?php include '../inc/header.php'; ?>
 
 <main class="container">
-    <h1>Produto</h1>
+    <h1 class="mt-3 text-center">Produto</h1>
 
     <hr>
+
+    <div class="card">
+        <img src="../assets/img/<?= $produto['imagem']; ?>" alt="" class="card-img-top">
+
+        <div class="card-header">
+            <h5 class="card-title">
+                <?= $produto['nome']; ?>
+            </h5>
+        </div>
+
+        <div class="card-body">
+            <p class="card-text">
+                <?= $produto['descricao']; ?>
+            </p>
+
+            <p class="card-text">
+            <b>R$: </b><?= $preco; ?>
+            </p>
+        </div>
+
+        <div class="card-footer text-center">
+            <a href="../index.php" class="btn btn-success">Voltar</a>
+        </div>
+    </div>
 </main>
 
 <?php include '../inc/footer.php'; ?>
